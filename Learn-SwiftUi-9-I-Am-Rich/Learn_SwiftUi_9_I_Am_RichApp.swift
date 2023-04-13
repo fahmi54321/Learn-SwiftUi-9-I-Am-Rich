014d0574d278972e3265873dad7fc4ad83106d71
fahmi <fahmiabdulaziz44@gmail.com>
2023-04-13 15:51:36 +0700

    Initial Commit

---
+++ Learn-SwiftUi-9-I-Am-Rich/Learn_SwiftUi_9_I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUi_9_I_Am_RichApp.swift
+//  Learn-SwiftUi-9-I-Am-Rich
+//
+//  Created by Fahmi Aziz on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUi_9_I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
